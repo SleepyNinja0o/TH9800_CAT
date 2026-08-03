7cd2772b2928b4509c932a84941f57981aeb01fa
SleepyNinja <vb6email@gmail.com>
2026-08-03 00:01:10 -0400

    Add option to fix armv6l in run.sh

--- run.sh
+++ run.sh
@@ -1,3 +1,18 @@
 #!/usr/bin/env bash
+ARCH="$(uname -m)"
+
 cd "$(dirname "${BASH_SOURCE[0]}")"
-exec "$(dirname "${BASH_SOURCE[0]}")/venv/bin/python" TH9800_CAT.py "$@"
+
+case "$ARCH" in
+    armv6l)
+        exec "$(dirname "${BASH_SOURCE[0]}")/venv/bin/python" TH9800_CAT.py "$@"
+        #exec "MESA_GL_VERSION_OVERRIDE=4.5 MESA_GLSL_VERSION_OVERRIDE=450 $(dirname "${BASH_SOURCE[0]}")/venv/bin/python" TH9800_CAT.py "$@"
+        ;;
+    armv7l|aarch64|arm64)
+        exec "$(dirname "${BASH_SOURCE[0]}")/venv/bin/python" TH9800_CAT.py "$@"
+        ;;
+    *)
+        echo "Unknown architecture ($ARCH)..."
+        exit 1
+        ;;
+esac
